75160927f282164f6e00202e268d4f709d295cda
AdB <31643117+github4pxl@users.noreply.github.com>
2017-11-26 18:13:55 +1000

    Create centos_default_build_vps.sh

---
+++ centos_default_build_vps.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Version 0.1 24/11/2017
+# Author 'AdB'
+# This scripts installs my must-have packages on Vultr servers
+#
+# Preparing environment...
+# Set correct hostname
+hostnamectl set-hostname hostname.domain.com
+ 
+# Install basic tools
+yum update && yum upgrade
+yum -y install gpasswd net-tools bind-utils sudo links gcc git nmap wget curl telnet p7zip zip tcpdump rkhunter ntp nano joe vim python34-setuptools redis haveged
+ 
+# Easy install
+sudo easy_install-3.4 pip
+ 
+# yum whatprovides <app_name>
+ 
+# Postfix is optional
+#yum -y install postfix dovecot
+ 
+# Configure secure SSH logon
+adduser pxlusr
+passwd pxlusr
+gpasswd -a pxlusr wheel
+mkdir /home/pxlusr/.ssh
+cp .ssh/authorized_keys /home/pxlusr/.ssh/
+cd /home/pxlusr/.ssh/
+chown pxlusr:pxlusr authorized_keys
+ 
+# Modify sshd_config file followed by service restart
+systemctl restart sshd.service
+ 
+# Run Rootkit Hunter
+rkhunter --check
+ 
+# Install and configure SElinux Policy
+yum -y install selinux-policy
+getenforce
+setenforce 0 [OR 1]
+ 
+# Other dependencies and additional RedHat repos
+yum -y install epel-release centos-release-scl
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+yum -y install python-pip python3-dev python3-pip libpq5 libjpeg-dev python-devel
+ 
+# Optional for MISP
+# yum -y install httpd mariadb mariadb-server libxslt-devel zlib-devel
+# pear install Crypt_GPG
